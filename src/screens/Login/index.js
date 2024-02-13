d00b4f3e58be43de9f300d08b5586e1b39228bb7
Lucas Gola da Silva <lucasgola@outlook.com>
2024-02-13 15:23:43 -0300

    [add] login screen (not stylized)

---
+++ src/screens/Login/index.js
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import {
+    TextInput, View, StyleSheet, Text,
+    SafeAreaView, StatusBar, Button
+} from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const App = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    return (
+        <SafeAreaView
+            style={styles.mainContainer}
+        >
+            <StatusBar
+                backgroundColor="#61dafb"
+            />
+            <View style={styles.titleView}>
+                <Text style={styles.titleText}>Login</Text>
+            </View>
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Insira seu endereço de e-mail"
+                    value={email}
+                    onChangeText={setEmail}
+                    placeholderTextColor="#aaa"
+                />
+            </View>
+            <View style={styles.container}>
+                <TextInput
+                    secureTextEntry={!showPassword}
+                    value={password}
+                    onChangeText={setPassword}
+                    style={styles.input}
+                    placeholder="Insira sua senha"
+                    placeholderTextColor="#aaa"
+                />
+                <MaterialCommunityIcons
+                    name={showPassword ? 'eye-off' : 'eye'}
+                    size={24}
+                    color="#aaa"
+                    style={styles.icon}
+                    onPress={toggleShowPassword}
+                />
+            </View>
+            <Text
+                style={styles.button}
+                onPress={() => console.log('Botão de login')}
+            >
+                Login
+            </Text>
+            <View style={styles.cadastroView}>
+                <Text
+                    style={styles.cadastroText}
+                    onPress={() => console.log('Botão de cadastro')}
+                >
+                    Cadastre-se
+                </Text>
+            </View>
+        </SafeAreaView >
+    );
+};
+
+const styles = StyleSheet.create({
+    titleView: {
+        paddingBottom: 30,
+    },
+    titleText: {
+        fontSize: 20,
+    },
+    mainContainer: {
+        marginTop: 70,
+        margin: 40,
+    },
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#f3f3f3',
+        borderRadius: 8,
+        paddingHorizontal: 14,
+        marginBottom: 20,
+    },
+    input: {
+        flex: 1,
+        color: '#333',
+        paddingVertical: 10,
+        paddingRight: 10,
+        fontSize: 16,
+    },
+    icon: {
+        marginLeft: 10,
+    },
+    heading: {
+        alignItems: 'center',
+        fontSize: 20,
+        color: 'green',
+        marginBottom: 20,
+    },
+    button: {
+        textAlign: 'center',
+        fontSize: 15,
+        padding: 10,
+        backgroundColor: "#61dafb",
+        borderRadius: 20,
+    },
+    cadastroView: {
+        marginTop: 25,
+        alignItems: 'center'
+    },
+    cadastroText: {
+        color: "#61dafb"
+    }
+});
+
+export default App;
