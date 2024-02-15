91a9f7178f3265fae849819cb18a468f990922e6
Lucas Gola da Silva <lucasgola@outlook.com>
2024-02-15 06:21:54 -0300

    [wip] imagem e título (nº do pedido)

---
+++ src/screens/Orders/Orders Screen.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, SafeAreaView, StatusBar, Image, } from "react-native";
+import timeline from '../../mocks/timeline.json'
+import _ from "lodash";
+import images from "../../assets";
+
+const OrderCard = () => {
+    return (
+        <SafeAreaView style={styles.mainContainer}>
+            <StatusBar
+                backgroundColor="#61dafb"
+            />
+            {timeline.cards.map((card) => {
+                return (
+                    <View style={styles.card}>
+                        <Image source={images[card.jogo]} style={styles.image} />
+                        <Text style={styles.cardTitle}>Pedido Nº: {card.cardId}</Text>
+                    </View>
+                )
+            })}
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    card: {
+        flexDirection: 'row',
+        padding: 10,
+        margin: 5,
+        borderRadius: 5,
+        borderColor: 'grey',
+        borderWidth: 1
+    },
+    mainContainer: {
+        marginTop: 50,
+        margin: 30,
+    },
+    cardTitle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlignVertical: 'center'
+    },
+    timeline: {
+        margin: 10,
+    },
+    timelineText: {
+        color: '#fff'
+    },
+    image: {
+        width: 50,
+        height: 50,
+        marginRight: 20,
+        margin: 5
+    },
+});
+
+export default OrderCard;
